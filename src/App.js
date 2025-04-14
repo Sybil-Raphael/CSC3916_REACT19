19e0ecfde52e440a99197f290f39b71921d9d18c
Sybil Raphael <sybil.raphael@ucdenver.edu>
2025-04-14 10:01:05 -0600

    delete changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import MovieList from './components/movielist';
 import Movie from './components/movie';
 import Authentication from './components/authentication';
 import {HashRouter, Routes,  Route} from 'react-router-dom';
-import MovieDetail from './components/moviedetail';
-
 
 function App() {
   return (
@@ -15,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MovieList />} />
           <Route path="/movielist" element={<MovieList />}/>
-          <Route path="/movie/:movieId" element={<MovieDetail />}/>
+          <Route path="/movie/:movieId" element={<Movie />}/>
           <Route path="/signin" element={<Authentication />}/>
           {/*... other routes */}
         </Routes>
